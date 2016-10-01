5a1b07910265e4ab7c09e124af90906de590ca29
theWindCoder <zanyang1103@163.com>
2016-10-02 00:07:11 +0800

    增加游戏结束时restartbutton的样式的修改，成功和失败分别显示不同icon

---
+++ Assets/script/ChangeRestartButton.cs
@@ -0,0 +1,48 @@
+/*********************************************************************************
+ *Copyright(C) 2015 by DefaultCompany
+ *All rights reserved.
+ *FileName:     ChangeRestartButton.cs
+ *Author:       DefaultCompany
+ *Version:      1.0
+ *UnityVersion：5.4.1f1
+ *Date:         2016-10-01
+ *Description:   
+ *History:  
+**********************************************************************************/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeRestartButton : MonoBehaviour
+{
+    GameManager gameManager;
+    public Image restartImg;
+    public Sprite successSprite;
+    public Sprite failedSprite;
+    public Sprite normalSprite;
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameManager.instance;
+        gameManager.GameFinishEvent += onGameFinish;
+    }
+    /// <summary>
+    /// 游戏结束时修改一下按钮的样式
+    /// </summary>
+    /// <param name="finishtype"></param>
+    void onGameFinish(string finishtype)
+    {
+        if (finishtype.ToLower().Equals("success"))
+        {
+            restartImg.sprite = successSprite;
+        }
+        else if (finishtype.ToLower().Equals("failed"))
+        {
+            restartImg.sprite = failedSprite;
+        }
+    }
+    void OnDestroy()
+    {
+        gameManager.GameFinishEvent -= onGameFinish;
+    }
+}
